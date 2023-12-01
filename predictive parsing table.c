de0da351c458849578f8629e8503263cc7be97ec
prudhviyadala <122338807+prudhviyadala@users.noreply.github.com>
2023-12-01 08:37:52 +0530

    Create predictive parsing table.c

---
+++ predictive parsing table.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+bool isVariableUsed(int variableValue) {
+   
+    return variableValue > 0;
+}
+
+void eliminateDeadCode(int* variable1, int* variable2, int* variable3) {
+    if (!isVariableUsed(*variable1)) {
+        *variable1 = 0; // Mark as dead code
+    }
+    if (!isVariableUsed(*variable2)) {
+        *variable2 = 0; // Mark as dead code
+    }
+    if (!isVariableUsed(*variable3)) {
+        *variable3 = 0; // Mark as dead code
+    }
+}
+
+int main() {
+    int var1 = 5;
+    int var2 = 0;
+    int var3 = -10;
+
+       eliminateDeadCode(&var1, &var2, &var3);
+
+    printf("Variable 1: %d\n", var1);
+    printf("Variable 2: %d\n", var2);
+    printf("Variable 3: %d\n", var3);
+
+    return 0;
+}
